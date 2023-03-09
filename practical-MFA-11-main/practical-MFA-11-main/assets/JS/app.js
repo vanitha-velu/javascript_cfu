c646168b553b673120b14cbbca53c53537f36e49
vanitha-velu <118425906+vanitha-velu@users.noreply.github.com>
2023-03-09 18:37:47 +0530

    commit

---
+++ practical-MFA-11-main/practical-MFA-11-main/assets/JS/app.js
@@ -0,0 +1,12 @@
+
+/*get the number of characters*/
+function charLength() {
+
+    let charLength = document.getElementById('length').value;
+    document.getElementById('result').innerHTML = charLength;
+    return charLength;
+
+}
+
+document.getElementById('length').addEventListener('change', charLength)
+
